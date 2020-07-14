f1bc85cd0588ee03154fb9a631c21afa72ca83e1
Grzegorz Pawlik <grzegorzpawlik@gmail.com>
2020-07-14 15:55:48 +0200

    fixup! Challenge Part One, 3

--- part_one/challenges_1.4/1+2+3.rb
+++ part_one/challenges_1.4/1+2+3.rb
@@ -30,3 +30,19 @@ typeof_after_id = compose.(
 print '> typeof_after_id.("123") => '
 puts typeof_after_id.("123")
 
+puts <<STR
+3.Write a program that tries to test that your composition function
+respects identity.
+STR
+
+id_after_typeof = compose.(
+  id, get_type
+)
+
+test_composition = lambda do |f1, f2, object|
+  compose.(f1, f2).(object) == compose.(f2, f1).(object)
+end
+
+[1, 7, 42, "yes", true, nil].each do |object|
+  puts test_composition.(id, get_type, object)
+end
